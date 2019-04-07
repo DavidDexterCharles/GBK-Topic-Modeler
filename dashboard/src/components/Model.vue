afd365fc3f50aa6dd2bca89c0fe5e450c40de8df
David <david.dexter.charles@gmail.com>
2019-04-07 16:27:43 +0000

    gbk frontend

---
+++ dashboard/src/components/Model.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Basic textarea</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="15"></textarea>
+  </div>
+</template>
---
+++ dashboard/src/components/Test.vue
@@ -0,0 +1,41 @@
+<template>
+  <section id="tables">
+    <h1>apples</h1>
+     <mdb-btn color="primary">Primary</mdb-btn>
+      <form class="md-form">
+             <input type="file">
+        </form>
+  </section>
+</template>
+
+<script>
+import { mdbBtn, mdbRow, mdbCol, mdbCard, mdbView, mdbCardBody, mdbTbl } from 'mdbvue'
+
+export default {
+  name: 'Tables',
+  components: {
+    mdbBtn,
+    mdbRow,
+    mdbCol,
+    mdbCard,
+    mdbView,
+    mdbCardBody,
+    mdbTbl
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.card.card-cascade .view.gradient-card-header {
+  padding: 1rem 1rem;
+  text-align: center;
+}
+.card.card-cascade h3,
+.card.card-cascade h4 {
+  margin-bottom: 0;
+}
+</style>
