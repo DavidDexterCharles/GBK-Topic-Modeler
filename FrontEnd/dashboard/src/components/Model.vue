22f7adb5387da4ac635034bc2c3f5c94743f3135
David <david.dexter.charles@gmail.com>
2019-04-08 15:03:04 +0000

    frontend update

--- FrontEnd/dashboard/src/components/Model.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="form-group">
-    <label for="exampleFormControlTextarea1">Basic textarea</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="15"></textarea>
-  </div>
-</template>
