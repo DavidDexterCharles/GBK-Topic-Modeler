38f856276ba363d67df9235feb26fad24736ed33
David <david.dexter.charles@gmail.com>
2019-04-08 15:05:50 +0000

    frontend

---
+++ FrontEnd/dashboard/src/components/Classifiers.vue
@@ -0,0 +1,9 @@
+<template>
+  <section id="">
+    <h1>apples</h1>
+     <mdb-btn color="primary">Primary</mdb-btn>
+      <form class="md-form">
+             <input type="file">
+        </form>
+  </section>
+</template>
